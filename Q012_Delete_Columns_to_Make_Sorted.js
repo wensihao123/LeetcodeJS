08dfc2cd7231a27bf65990bae0098ffd526c8174
Wen Sihao <wensihao822@gmail.com>
2019-03-19 17:58:25 +0800

    Update

--- Q012_Delete_Columns_to_Make_Sorted.js
+++ Q012_Delete_Columns_to_Make_Sorted.js
@@ -2,6 +2,18 @@
  * @param {string[]} A
  * @return {number}
  */
-var minDeletionSize = function(A) {
-    
+var minDeletionSize = function (A) {
+    let table = []
+    count = 0
+    A.forEach((word, i) => {
+        word.split('').forEach((char, j) => {
+            if (table[j]) {
+                if (i > 0 && char < table[j][i - 1]) count++
+                else if (table[j][i - 1]) table[j] += char
+            } else table[j] = char
+        })
+    })
+    return count
 };
+
+console.log(minDeletionSize(["zyx","wvu","tsr"]))
