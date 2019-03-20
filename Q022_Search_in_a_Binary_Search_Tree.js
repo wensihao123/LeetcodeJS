c3813eb400db8e7b2c576ea0958a5de79264229e
Wen Sihao <wensihao822@gmail.com>
2019-03-20 17:58:09 +0800

    minor changes

--- Q022_Search_in_a_Binary_Search_Tree.js
+++ Q022_Search_in_a_Binary_Search_Tree.js
@@ -10,6 +10,28 @@
  * @param {number} val
  * @return {TreeNode}
  */
+const root  = {
+    'val' : 2,
+    'left' : {
+        'val': 1,
+        'left': null,
+        'right': {
+            'val' : 3,
+            'left': null,
+            'right': null
+        }
+    },
+    'right' : {
+        'val': 4,
+        'left': null,
+        'right': {
+            'val' : 5,
+            'left': null,
+            'right': null
+        }
+    }
+}
+
 var searchBST = function(root, val) {
     
 };
